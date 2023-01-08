6558b66a29475f89a8c598e5b462d91adbbc4870
arrzdev <andre.arroz.santos@gmail.com>
2023-01-08 06:05:52 +0000

    Implemented clients CLI

--- subscriber/sub.c
+++ subscriber/sub.c
@@ -1,9 +1,24 @@
 #include "logging.h"
+#include "wire_protocol.h"
 
-int main(int argc, char **argv) {
-    (void)argc;
-    (void)argv;
-    fprintf(stderr, "usage: sub <register_pipe_name> <box_name>\n");
-    WARN("unimplemented"); // TODO: implement
+#define OP_CODE 2
+
+int main(int argc, char **argv)
+{
+  if (argc != 4)
+  {
+    WARN("number of arguments invalid");
     return -1;
+  }
+
+  // char *register_pipe_name = argv[1];
+  char *client_pipe_name = argv[2];
+  char *box_name = argv[3];
+
+  char wire_message[MAX_WIRE_MESSAGE_SIZE];
+  snprintf(wire_message, MAX_WIRE_MESSAGE_SIZE, "%d|%s|%s", OP_CODE, client_pipe_name, box_name);
+
+  printf("Subscriber Sent: %s\n", wire_message);
+
+  return 0;
 }
